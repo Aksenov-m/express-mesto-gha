b834d1052cf03d0d46faf12501a1ea0ee96267aa
Aksenov-m <aksenov.m87@yandex.ru>
2022-04-13 00:50:34 +0300

    feat: добавить схемы и модели

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+// подключаемся к серверу mongo
+mongoose.connect("mongodb://localhost:27017/mestodb");
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
---
+++ models/card.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String, // имя карточки — это строка
+    required: true, // оно должно быть у каждого пользователя, так что имя — обязательное поле
+    minlength: 2, // минимальная длина имени — 2 символа
+    maxlength: 30, // а максимальная — 30 символов
+  },
+  link: {
+    type: String, // имя — это строка
+    required: true, // обязательное поле
+    ref: "link",
+  },
+  owner: {
+    type: mongoose.Types.ObjectId, // ссылка на модель автора карточки
+    required: true, // обязательное поле
+    ref: "owner",
+  },
+  likes: {
+    type: mongoose.Types.ObjectId, // ссылка на модель автора карточки
+    default: [], // пустой массив
+  },
+  createdAt: {
+    type: Date, // дата создания
+    default: Date.now(),
+  },
+});
+// создаём модель и экспортируем её
+module.exports = mongoose.model("card", cardSchema);
